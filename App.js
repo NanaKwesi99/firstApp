42d9b165237648b5d2948f0c6f173e6166f6a4cd
Weggins <Emmanuelmensah735@gmail.com>
2021-11-25 02:40:23 +0000

    All done

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.profileContainer}>
+      <Image source={require("./assets/photo.jpeg")} style={styles.image} />
+      </View>
+      <View style={styles.input}>
+        <TextInput placeholder="Name" style={styles.inputContainer} />
+        <TextInput placeholder="Email" style={styles.inputContainer} />
+        <TextInput placeholder="Password" style={styles.inputContainer} />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
   },
+
+  profileContainer: {
+    flex: 1,
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: 'center'
+  },
+  input: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  inputContainer: {
+    flex: 1,
+    borderWidth: 1,
+    alignItems: "center",
+    width: 350,
+    justifyContent: "center",
+    borderRadius: 25,
+  },
+  image: {
+    height: 150,
+    width: 150,
+    borderRadius: 100,
+
+  }
 });
